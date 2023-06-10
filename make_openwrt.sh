57c3fd40ce34491339443ef13e48dc23416074e9
wise Feng <419194314@qq.com>
2023-06-11 02:09:22 +0800

    以纯净版运行

--- make_openwrt.sh
+++ make_openwrt.sh
@@ -78,8 +78,8 @@ if [ "$clean_build" = true ]; then
   make clean
 fi
 
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
